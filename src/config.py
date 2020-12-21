70480894bf935314618b14d3a39ae8632e011101
muzilli <mmuzilli@findmee.com.br>
2020-12-21 10:05:11 -0300

    Initial commit

--- src/config.py
+++ src/config.py
@@ -19,11 +19,12 @@
 # unique to your accounts. These are used when constructing connection strings
 # for the Batch and Storage client objects.
 
-_BATCH_ACCOUNT_NAME = ''
-_BATCH_ACCOUNT_KEY = ''
-_BATCH_ACCOUNT_URL = ''
-_STORAGE_ACCOUNT_NAME = ''
-_STORAGE_ACCOUNT_KEY = ''
+_BATCH_ACCOUNT_NAME = 'batrseus001'  # Your batch account name
+_BATCH_ACCOUNT_KEY = 'KZR44AGFgFDG25GWpyHEZ6LMBfWTht1j9s5NsI1ojlMRk20LFmqFnheBV0LhAS7m+7vFnfDgVoYGL2nr5oWjPg=='  # Your batch account key
+_BATCH_ACCOUNT_URL = 'https://batrseus001.eastus.batch.azure.com'  # Your batch account URL
+_STORAGE_ACCOUNT_NAME = 'strseus001'  # Your storage account name
+_STORAGE_ACCOUNT_KEY = 'D6F1VeB6QHLQMiiZN4X2rGWpbtW+XbFi6SIwCsDtPqCE2uDbn6BbY30Ruf7LWZlz+4kowsjf+BCeuLfk5D2tag=='  # Your storage account key
+_STANDARD_OUT_FILE_NAME = 'stdout.txt'  # Standard Output file
 _POOL_ID = 'LinuxFfmpegPool'
 _DEDICATED_POOL_NODE_COUNT = 0
 _LOW_PRIORITY_POOL_NODE_COUNT = 5
