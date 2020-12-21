b177548a0e7612c28e669efa2a0771bf77b053c0
muzilli <mmuzilli@findmee.com.br>
2020-12-21 15:59:32 -0300

    Teste

--- main.py
+++ main.py
@@ -3,6 +3,13 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+# Imports the Google Cloud client library
+from google.cloud import vision
+
+import cv2
+import numpy as np
+import io
+import os
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
